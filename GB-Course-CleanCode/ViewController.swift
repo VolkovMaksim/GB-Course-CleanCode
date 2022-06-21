a60b6141e85f3269eeb0e6dff49359e6b67c6d4d
Maksim Volkov <volkovmd.dev@mail.ru>
2022-06-21 16:04:32 +0300

    Initial Commit

---
+++ GB-Course-CleanCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GB-Course-CleanCode
+//
+//  Created by Maksim Volkov on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
